71fb9f0fa7b2a23c57d9b9833a077f3eff41f546
nolleto <felipenolletonascimento@gmail.com>
2019-02-25 21:16:36 -0300

    Updates

---
+++ precache-manifest.83bf1b7dad033d95ba57a66241af68e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7243a511b0eb6282d1652d7021da1798",
+    "url": "/abastece-ai-custo-beneficio-app/static/media/logo.7243a511.svg"
+  },
+  {
+    "revision": "8502af4c973b8af077fc",
+    "url": "/abastece-ai-custo-beneficio-app/static/js/runtime~main.8502af4c.js"
+  },
+  {
+    "revision": "0b3852b94ce395ba6a3e",
+    "url": "/abastece-ai-custo-beneficio-app/static/js/main.0b3852b9.chunk.js"
+  },
+  {
+    "revision": "31e1a973e9a0260cc554",
+    "url": "/abastece-ai-custo-beneficio-app/static/js/2.31e1a973.chunk.js"
+  },
+  {
+    "revision": "0b3852b94ce395ba6a3e",
+    "url": "/abastece-ai-custo-beneficio-app/static/css/main.87071111.chunk.css"
+  },
+  {
+    "revision": "d4ed6f767f9a09aa8d8ebabdf736d5c7",
+    "url": "/abastece-ai-custo-beneficio-app/index.html"
+  }
+];
